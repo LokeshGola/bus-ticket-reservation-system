49a2b5e62a2b748fab77cb5fb8d18382a2f66aaa
LokeshGola <lokeshgola121@gmail.com>
2023-09-28 13:48:09 +0530

    UI updated

--- Bus_Ticket_Reservation_System/src/com/masaischool/ui/UIMain.java
+++ Bus_Ticket_Reservation_System/src/com/masaischool/ui/UIMain.java
@@ -2,21 +2,22 @@ package com.masaischool.ui;
 
 import java.util.Scanner;
 
-
 public class UIMain {
 	
 	public static void displayAdminMenu(Scanner sc) {
-		System.out.println("Welcome Admin...!");
-		System.out.println("1. Add bus");
-		System.out.println("2. Update bus");
-		System.out.println("3. Delete bus");
-		System.out.println("4. View all bookings");
-		
-		System.out.println("5. View bookings for a date range");
-		System.out.println("6. View bookings by bus name");
-		System.out.println("7. View bookings by mobile number of a passenger");
-		System.out.println("0. Exit");
-		System.out.println("Enter Selection : ");
+		System.out.println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("  ^     Welcome Admin...!                                      ^");
+		System.out.println("  ^                                                            ^");
+		System.out.println("  ^     1. Add bus                                             ^");
+		System.out.println("  ^     2. Update bus                                          ^");
+		System.out.println("  ^     3. Delete bus                                          ^");
+		System.out.println("  ^     4. View all bookings                                   ^");
+		System.out.println("  ^     5. View bookings for a date range                      ^");
+		System.out.println("  ^     6. View bookings by bus name                           ^");
+		System.out.println("  ^     7. View bookings by mobile number of a passenger       ^");
+		System.out.println("  ^     0. Exit                                                ^");
+		System.out.println("  ^     Entre Selection :                                      ^");
+		System.out.println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 	public static void adminMenu(Scanner sc) {
 		int choice = 0;
@@ -36,7 +37,6 @@ public class UIMain {
 			case 4:
 				BusUI.viewAllBookings();
 				break;
-				
 			case 5:
 				BusUI.viewBookingsForDateRange(sc);
 				break;
@@ -56,9 +56,9 @@ public class UIMain {
 		}while(choice !=0);
 	}
 	public static void adminLogin(Scanner sc) {
-		System.out.println("Enter username:");
+		System.out.println("Entre username:");
 		String username = sc.next();
-		System.out.println("Enter password:");
+		System.out.println("Entre password:");
 		String password = sc.next();
 		if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("root")) {
 			adminMenu(sc);
@@ -67,16 +67,20 @@ public class UIMain {
 		}
 	}
 	public static void displayCustomerMenu(Scanner sc) {
-		System.out.println("Welcome User...!");
-		System.out.println("1. View bus list");
-		System.out.println("2. Book ticket by bus number");
-//		System.out.println("3. Cancel ticket");
-		System.out.println("3. Update customer");
-		
-		System.out.println("4. View booking history");
-		System.out.println("5. Delete account");
-		System.out.println("0. Log out");
-		System.out.println("Enter Selection : ");
+		System.out.println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("  ^     Welcome User...!                 ^");
+		System.out.println("  ^                                      ^");
+		System.out.println("  ^     1. View bus list                 ^");
+		System.out.println("  ^     2. Book ticket by bus number     ^");
+		System.out.println("  ^     3. Cancel ticket                 ^");
+		System.out.println("  ^     4. Update customer               ^");
+		System.out.println("  ^     5. View booking history          ^");
+		System.out.println("  ^     6. Delete account                ^");
+//		System.out.println("  ^     7. Update bus                    ^");
+//		System.out.println("  ^     8. Update bus                    ^");
+		System.out.println("  ^     0. Log out                       ^");
+		System.out.println("  ^     Entre Selection :                ^");
+		System.out.println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 	public static void customerMenu(Scanner sc) {
 		int choice = 0;
@@ -88,21 +92,20 @@ public class UIMain {
 				CustomerUI.viewBusList();
 				break;
 			case 2:
-				CustomerUI.bookTicket(sc);
+				CustomerUI.bookTicketByBusNumber(sc);
 				break;
-//			case 3:
-//				CustomerUI.cancelTicket(sc);
-//				break;
 			case 3:
-				CustomerUI.updateCustomer(sc);
+				CustomerUI.cancelTicket(sc);
 				break;
 			case 4:
-				CustomerUI.getBookingHistory();
+				CustomerUI.updateCustomer(sc);
 				break;
 			case 5:
+				CustomerUI.getBookingHistory();
+				break;
+			case 6:
 				CustomerUI.deleteAccount();
 				CustomerUI.logout();     // After deleting the account, called the log out method
-				choice = 0;
 				break;
 			case 0:
 				System.out.println("Thank you user. have a good day.");
@@ -127,14 +130,19 @@ public class UIMain {
 	}
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Welcome to Atlantic Express Bus Reservation...!");
+		System.out.println("  ============================================================");
+		System.out.println("  ((     Welcome to Atlantic Express Bus Reservation...!    ((");
+		System.out.println("  ))                                                        ))");
+		System.out.println("  ============================================================");
 		int choice = 0;
 		do {
-			System.out.println("1. Admin Login");
-			System.out.println("2. Customer Login");
-			System.out.println("3. Customer Sign up");
-			System.out.println("0. Exit");
-			System.out.println("Enter Selection : ");
+			System.out.println("  ==============================");
+			System.out.println("  ||  1. Admin Login          ||");
+			System.out.println("  ||  2. Customer Login       ||");
+			System.out.println("  ||  3. Customer Sign up     ||");
+			System.out.println("  ||  0. Exit                 ||");
+			System.out.println("  ||  Entre Selection :       ||");
+			System.out.println("  ==============================");
 			choice = sc.nextInt();
 			switch(choice) {
 			case 1:
